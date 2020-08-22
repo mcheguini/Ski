acbecc24162df657abc527f772fef6e2d1fc7460
Mazeyar <mazyar@protonmail.com>
2020-08-22 16:22:36 -0500

    Init Routine to read data

--- Program.cs
+++ Program.cs
@@ -7,38 +7,23 @@ namespace Ski {
     class Program {
         //Global Vars
         private const int maxElevation = 1500; // limit value
-        // private const int m = 4,
-        //     n = 4;
-        // static int[, ] map = new int[m, n] { { 4, 8, 7, 3 }, { 2, 5, 9, 3 }, { 6, 3, 2, 5 }, { 4, 4, 1, 6 }
-        // };
+        static int m=0, n=0; // matrix dimension
         private static int dS = 0; // Aux var to compute difference in Slope find maximum slope ()
         private static int len = 0, lenAux = 0; // to find maximum length
         private static bool north = false, east = false, south = false, west = false; // boolean to check if we move to any direction
         private static List<int> auxL = new List<int> (); // temp list to hold the search path results
         private static List<int> resultL = new List<int> (); //hold the result of the search (Longest path)
         static void Main (string[] args) {
-            string fileName = "4x4.txt"; // File Name
-            string path = System.IO.Directory.GetCurrentDirectory () + "/" + fileName; // Full path to the File Name I think In windows / will change to \
-            //Console.WriteLine(path); // Debug purpose
-            String input = File.ReadAllText (path); // Read all the Data from the text File save it into a 
-            String[] array = input.Split ('\n'); // Read the whole text into the string 
-            //List<int> auxL = new List<int> (); // 
-            int[] dimention = stringToIntArray (array[0], ' '); // Read Matrix dimension form the first Row
-            int m = dimention[0], n = dimention[1]; // Store the dimention into the variable 
-int[,] map = new int[m,n]; // create empty map matrix
+            int[,] map = readData(); // Read matrix data form the text file
+            Console.WriteLine ("Original Map:");
+            mapPrint (map, m, n); // debug
 
-
-            for (int i = 1; i < array.GetLength (0); i++) { // Read raw data pass to the matrix 
-                int[] row = stringToIntArray (array[i], ' ');
-                for (int j = 0; j < row.Length; j++) {
-                    map[i-1,j] = row[j];
-                }
-            }
-            Console.WriteLine("")
-            mapPrint(map,m,n); // debug
         }
+        // Desc: split string into the array
+        // INPUT: string and sep character
+        // OUTPUT: int array
         private static int[] stringToIntArray (string str, char sep) {
-            string[] aux = str.Split(sep);
+            string[] aux = str.Split (sep);
             int[] result = new int[aux.Length]; // create an aux Int Array same length
             for (int i = 0; i < result.Length; ++i) {
                 int j;
@@ -47,7 +32,27 @@ int[,] map = new int[m,n]; // create empty map matrix
             }
             return result;
         }
-
+// Desc: Read raw data from the text file and set the map matrix
+// OUTPUT: Map(Matrix)
+private static int[,] readData (){
+            string fileName = "4x4.txt"; // File Name
+            string path = System.IO.Directory.GetCurrentDirectory () + "/" + fileName; // Full path to the File Name I think In windows / will change to \
+            //Console.WriteLine(path); // Debug purpose
+            String input = File.ReadAllText (path); // Read all the Data from the text File save it into a 
+            String[] array = input.Split ('\n'); // Read the whole text into the string 
+            //List<int> auxL = new List<int> (); // 
+            int[] dimention = stringToIntArray (array[0], ' '); // Read Matrix dimension form the first Row
+            m = dimention[0];
+            n = dimention[1]; // Store the dimention into the variable 
+            int[, ] map = new int[m, n]; // create empty map matrix
+            for (int i = 1; i < array.GetLength (0); i++) { // Read raw data pass to the matrix 
+                int[] row = stringToIntArray (array[i], ' ');
+                for (int j = 0; j < row.Length; j++) {
+                    map[i - 1, j] = row[j];
+                }
+            }
+            return map;
+}
         // Desc: Construct the T/F based on the original matrix and range [0-1500]
         // INPUT: Original Matrix
         // Output: LogicMap 
